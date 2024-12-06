d67e17f35daca3f2a163f7f241864c1b7431975f
Bruno Reis <134024017+dev-brunoreis@users.noreply.github.com>
2024-12-06 12:09:52 -0300

    adjust doc and disable vue lsp

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 In manjaro, just clone this in `.config`
+
+```
+npm install -g intelephense
+```
--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -3,26 +3,26 @@ return {
     config = function()
       local lspconfig = require('lspconfig')
   
-      lspconfig.volar.setup {
-        filetypes = { "vue" },
-        settings = {
-          volar = {
-            languageFeatures = {
-              references = true,
-              definition = true,
-              typeDefinition = true,
-              hover = true,
-              rename = true,
-            },
-            completion = {
-              enable = true
-            },
-            diagnostic = {
-              enable = true
-            },
-          },
-        },
-      }
+    --   lspconfig.volar.setup {
+    --     filetypes = { "vue" },
+    --     settings = {
+    --       volar = {
+    --         languageFeatures = {
+    --           references = true,
+    --           definition = true,
+    --           typeDefinition = true,
+    --           hover = true,
+    --           rename = true,
+    --         },
+    --         completion = {
+    --           enable = true
+    --         },
+    --         diagnostic = {
+    --           enable = true
+    --         },
+    --       },
+    --     },
+    --   }
   
       lspconfig.intelephense.setup {
         settings = {
